d455d751e4dbd2dc57a9400702a6ee8901a9b5a0
chasem151 <90299333+chasem151@users.noreply.github.com>
2022-03-23 05:14:32 -0400

    New runtime instructions, general bash_submit.sh
    Generalized bash_submit.sh and added better runtime instructions in the README

--- CRAFT/CRAFT-pytorch-master/bash_submit.sh
+++ CRAFT/CRAFT-pytorch-master/bash_submit.sh
@@ -8,7 +8,7 @@ module load python3/3.8.10
 module load mxnet/1.7.0
 module load pytorch/1.10.2
 
-python test.py --trained_model=craft_mlt_25k.pth --test_folder=/projectnb/sparkgrp/ml-herbarium-angeline/ml-herbarium/in_data/images
+python test.py --trained_model=craft_mlt_25k.pth --test_folder=../../in_data/images
 
 ###if you want to run on cpu, run the below command line###
-#python test.py --trained_model=craft_mlt_25k.pth --test_folder=/projectnb/sparkgrp/ml-herbarium-angeline/ml-herbarium/in_data/images/..cuda=False
+#python test.py --trained_model=craft_mlt_25k.pth --test_folder=../../in_data/images/..cuda=False
