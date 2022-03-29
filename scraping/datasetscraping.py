f8fe33fa6ace59c1b36265b82fc0ddf2aa1c4f64
Eamon Niknafs <en@scc1.bu.edu>
2022-03-29 02:52:07 -0400

    fixed naming mistake

--- scraping/datasetscraping.py
+++ scraping/datasetscraping.py
@@ -13,9 +13,11 @@ import os
 import time
 import math
 import pandas
-import mimetypes
 import json
 import zipfile
+from tqdm import tqdm
+from glob import glob
+import multiprocessing as mp
 
 
 # %% [markdown]
@@ -24,15 +26,14 @@ import zipfile
 # %%
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
-PERCENT_TO_SCRAPE = 0.00002
+PERCENT_TO_SCRAPE = 0.00015
 NUMBER_TO_SKIP = 40000
 DATASET_PATH = "/projectnb/sparkgrp/ml-herbarium-data/"
 DATASET_ARCHIVE = "data.zip"
 DATASET_CSV = "data.csv"
 OUTPUT_PATH = "/projectnb/sparkgrp/ml-herbarium-data/scraped-data/" + timestr + "/"
-DATASET_URL = (
-    "https://api.gbif.org/v1/occurrence/download/request/0195391-210914110416597.zip"
-)
+DATASET_URL = "https://occurrence-download.gbif.org/occurrence/download/request/0196625-210914110416597.zip"
+NUM_CORES = min(mp.cpu_count(), 50)
 
 
 # %% [markdown]
@@ -41,13 +42,13 @@ DATASET_URL = (
 
 # %%
 def download_dataset():
-  if os.path.exists(DATASET_PATH+DATASET_ARCHIVE):
-      os.remove(DATASET_PATH+DATASET_ARCHIVE)
+  if os.path.exists(DATASET_PATH + DATASET_ARCHIVE):
+      os.remove(DATASET_PATH + DATASET_ARCHIVE)
   ds = requests.get(DATASET_URL, stream=True)
-  total_size_in_bytes=int(ds.headers.get('content-length', 0))
-  block_size = 1024 #1 Kibibyte
-  progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-  with open(DATASET_PATH+DATASET_ARCHIVE, "wb") as f:
+  total_size_in_bytes = int(ds.headers.get("content-length", 0))
+  block_size = 1024  # 1 Kibibyte
+  progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+  with open(DATASET_PATH + DATASET_ARCHIVE, "wb") as f:
       for data in ds.iter_content(block_size):
           progress_bar.update(len(data))
           f.write(data)
@@ -103,10 +104,15 @@ def close_dwca(dwca):
 
 # %%
 def save_csv_rows_to_pandas():
-    with zipfile.ZipFile(DATASET_PATH+DATASET_ARCHIVE, "r") as zip_ref:
-        zip_ref.extractall(DATASET_PATH+DATASET_CSV)
-    df = pandas.read_csv(DATASET_PATH+DATASET_CSV)
-    return df
+  if os.path.exists(DATASET_PATH + "*.csv"):
+      os.remove(DATASET_PATH + "*.csv")
+  with zipfile.ZipFile(DATASET_PATH + DATASET_ARCHIVE, "r") as zip_ref:
+      zip_ref.extractall(DATASET_PATH)
+  f = glob(os.path.join(DATASET_PATH, "*-*.csv"))[0]
+  os.rename(f, DATASET_PATH + DATASET_CSV)
+  print("CSV file extracted")
+  df = pandas.read_csv(DATASET_PATH + DATASET_CSV, sep="\t")
+  return df
 
 
 # %% [markdown]
@@ -134,45 +140,58 @@ def export_gbif_urls(df):
   NUMBER_TO_SCRAPE = math.ceil(df.shape[0] / NUMBER_TO_SKIP)
   print(str(NUMBER_TO_SCRAPE) + " IDs will be scraped.")
   for i in range(1, df.shape[0], NUMBER_TO_SKIP):
-      id = df.at[i, 'id']
+      if args[0] == 'dwca':
+          id = df.at[i, 'id']
+      elif args[0] == 'csv':
+          id = df.at[i, 'gbifID']
       data[i] = {'id':str(id)}
   print('Successfully scraped ' + str(len(data)) + ' IDs.')
   return data
 
+# %%
+def scrape_occurrence(key):
+  rq = requests.get("https://api.gbif.org/v1/occurrence/" + str(data[key]["id"]))
+  return_dict = {}
+  return_dict[key] = {}
+  return_dict[key]["img_url"] = json.loads(rq.content)["media"][0]["identifier"]
+  return_dict[key]["img_type"] = json.loads(rq.content)["media"][0]["format"]
+  return_dict[key]["country"] = json.loads(rq.content)["country"]
+  return_dict[key]["genus"] = json.loads(rq.content)["genus"]
+  return_dict[key]["species"] = json.loads(rq.content)["species"]
+  return return_dict
 # %% [markdown]
 # ### Fetch Image URLs and Specimen Data from GBIF API
+def fetch_images(data):
+  print("Starting multiprocessing...")
+  pool = mp.Pool(NUM_CORES)
+  print("Fetching data...")
+  for item in tqdm(pool.imap(scrape_occurrence, data), total=len(data)):
+      data.update(item)
+  pool.close()
+  pool.join()
 
-# %%
-def fetch_gbif_images(data):
-  print('Data will be fetched for', len(data), 'occurrences.')
-  i = 1
-  for idx in data:
-      print("\rProgress: " + str(i)+'/'+str(len(data)), end="")
-      rq = requests.get('https://api.gbif.org/v1/occurrence/' + str(data[idx]['id']))
-      data[idx]['img_url']=(json.loads(rq.content)['media'][0]['identifier'])
-      data[idx]['img_type']=(json.loads(rq.content)['media'][0]['format'])
-      data[idx]['country']=(json.loads(rq.content)['country'])
-      data[idx]['genus']=(json.loads(rq.content)['genus'])
-      data[idx]['species']=(json.loads(rq.content)['species'])
-      i+=1
-  print('\nSuccessfully fetched data for', len(data), 'occurrences.')
+  print("\nSuccessfully fetched data for", len(data), "occurrences.")
   return data
 
+# %%
+def download(key):
+  img = requests.get(data[key]["img_url"], stream=True)
+  with open(
+      OUTPUT_PATH + str(key) + "." + data[1]["img_type"].split("/", 1)[1], "wb"
+  ) as f:
+      shutil.copyfileobj(img.raw, f)
 # %% [markdown]
 # ### Download Images
-
-# %%
 def download_images(data):
-  i=1
   if not os.path.exists(OUTPUT_PATH):
-      os.makedirs(OUTPUT_PATH)
-  for idx in data:
-      img = requests.get(data[idx]['img_url'], stream=True)
-      with open(OUTPUT_PATH+str(idx)+mimetypes.guess_extension(data[idx]['img_type']),'wb') as f:
-          shutil.copyfileobj(img.raw, f)
-      print("\rProgress: " + str(i)+'/'+str(len(data)), end="")
-      i+=1
+    os.makedirs(OUTPUT_PATH)
 
+  print("Starting multiprocessing...")
+  pool = mp.Pool(NUM_CORES)
+  print("Downloading images...")
+  for _ in tqdm(pool.imap(download, data), total=len(data)):
+      pass
+  pool.close()
 
 # %% [markdown]
 # ### Export Geograpy Data
@@ -201,8 +220,9 @@ def print_help_message():
   print("Please specify a dataset to type using: 'python3 datasetscraping.py <dataset_type> [OPTIONAL ARGS]' where dataset_type is either 'dwca' or 'csv'.")
   print("\nOptional arguments:")
   print("\t-o, --output_path: Specify the output path for the images. Default is './output/'.")
-  print("\t-p, --percent_to_scrape: Specify the percentage of the dataset to scrape. Default is 0.1.")
+  print("\t-p, --percent_to_scrape: Specify the percentage of the dataset to scrape. Default is 0.00015 (~1198 occurrences).")
   print("\t-u, --dataset_url: Specify the dataset URL to download a new dataset.")
+  print("\t-c, --num_cores: Specify the number of cores to use. Default is 50.")
   print("\t-h, --help: Print this help message.")
   sys.exit(1)
 # %% [markdown]
@@ -214,17 +234,27 @@ if __name__ == "__main__":
     print_help_message()
   if args[0] == '-h' or args[0] == '--help' or args[0] != 'dwca' and args[0] != 'csv':
     print_help_message()
-  if args.index('-o') > 0:
+  if args.count('-o') > 0:
     OUTPUT_PATH = args[args.index('-o')+1]
-  if args.index('-p') > 0:
+  if args.count('-p') > 0:
     PERCENT_TO_SCRAPE = float(args[args.index('-p')+1])
-  if args.index('-u') > 0:
+  if args.count('-u') > 0:
     DATASET_URL = args[args.index('-u')+1]
+  if args.count('-c') > 0:
+    NUM_CORES = int(args[args.index('-c')+1])
+  if args.count('--output_path') > 0:
+    OUTPUT_PATH = args[args.index('--output_path')+1]
+  if args.count('--percent_to_scrape') > 0:
+    PERCENT_TO_SCRAPE = float(args[args.index('--percent_to_scrape')+1])
+  if args.count('--dataset_url') > 0:
+    DATASET_URL = args[args.index('--dataset_url')+1]
+  if args.count('--num_cores') > 0:
+    NUM_CORES = int(args[args.index('--num_cores')+1])
   if args[0] == 'dwca':
     print('Scraping dataset from Darwin Core Archive.')
     print('Output path: ' + OUTPUT_PATH)
     print('Opening Darwin Core Archive...')
-    dwca = open_dwca(DATASET_PATH+DATASET_ARCHIVE)
+    dwca = open_dwca()
     print('Successfully opened Darwin Core Archive.')
     print('Saving rows to Pandas DataFrame...')
     df = save_dwca_rows_to_pandas(dwca)
@@ -232,7 +262,6 @@ if __name__ == "__main__":
     print('Successfully saved rows to Pandas DataFrame.')
     print('Exporting data to output path...')
     data = export_gbif_urls(df)
-    data = fetch_gbif_images(data)
     download_images(data)
     export_geography_data(data)
     export_taxon_data(data)
@@ -241,11 +270,10 @@ if __name__ == "__main__":
     print('Scraping dataset from CSV.')
     print('Output path: ' + OUTPUT_PATH)
     print('Opening CSV...')
-    df = save_csv_rows_to_pandas(DATASET_PATH+DATASET_ARCHIVE)
+    df = save_csv_rows_to_pandas()
     print('Successfully saved CSV rows to Pandas DataFrame.')
     print('Exporting data to output path...')
     data = export_gbif_urls(df)
-    data = fetch_gbif_images(data)
     download_images(data)
     export_geography_data(data)
     export_taxon_data(data)
