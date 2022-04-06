6b7028c30baf82cd7c24ad9c2913eb2f2f932abf
Chase Maivald <chase1@scc1.bu.edu>
2022-04-06 04:33:12 -0400

    updated paths, started dictionaries

--- transcription/transcribe_labels.py
+++ transcription/transcribe_labels.py
@@ -46,11 +46,10 @@ ctx = mx.gpu(0) if mx.context.num_gpus() > 0 else mx.cpu()
 
 
 def get_imgsAndBoxes():
-	craft_res_dir = "../CRAFT/CRAFT-pytorch-master/result/"
-	org_img_dir = "../in_data/images/"
-	boxes = []
-	imgs = []
-	fnames = []
+	craft_res_dir = "/projectnb/sparkgrp/ml-herbarium-grp/ml-herbarium-data/CRAFT-results/20220405-014212/"
+	org_img_dir = "/projectnb/sparkgrp/ml-herbarium-grp/ml-herbarium-data/scraped-data"
+	boxes = {}
+	imgs = {}
 
 	for fname in sorted(os.listdir(craft_res_dir)):
 		if ".jpg" in fname and "mask" not in fname:
@@ -58,13 +57,12 @@ def get_imgsAndBoxes():
 			tmp_txt = open(os.path.join(craft_res_dir, fname[:len(fname)-3]+"txt"),"r").read().split("\n")[:-1]
 			tmp_txt = [line.split(",") for line in tmp_txt]
 			tmp_bxs = [[[int(line[i]),int(line[i+1])] for i,val in enumerate(line) if int(i)%2==0] for line in tmp_txt ]
-			boxes.append(tmp_bxs)
+			boxes[fname] = (tmp_bxs)
 
 	# get the original images to crop them
-	for fname in sorted(os.listdir(org_img_dir)):
+	for i in boxes:
 		if ".jpeg" in fname:
-			imgs.append(cv2.imread(os.path.join(org_img_dir, fname), cv2.IMREAD_GRAYSCALE))
-			fnames.append(fname)
+			imgs.append(cv2.imread(os.path.join(org_img_dir, i), cv2.IMREAD_GRAYSCALE))
 
 	return boxes,imgs,fnames
 
