3e0c3007005b1abd0ea01ce6b87e58eedd2b958d
Eamon Niknafs <en@scc1.bu.edu>
2022-04-06 03:41:46 -0400

    fixed label indexing using seg_label.py from commit with hash 8f269583330268774f05badca0aaff151e930688

--- segmentation/seg_label.py
+++ segmentation/seg_label.py
@@ -1,36 +1,30 @@
-import time
 import numpy as np
 import cv2
 import os
 import matplotlib.pyplot as plt
 
+
 # get the resulting images and text files
 craft_res_dir = "/projectnb/sparkgrp/ml-herbarium-grp/ml-herbarium-data/CRAFT-results/20220405-014212"
 # "/Users/jasonli/Desktop/BU/Junior/Spring2021/CS791/sandbox/test_models/CRAFT-pytorch-master/result"
-boxes = []
+boxes = {}
 # imgs = []
 
 for fname in sorted(os.listdir(craft_res_dir)):
-	if ".jpg" in fname and "mask" not in fname:
+	if ".txt" in fname and "mask" not in fname:
 		# imgs.append(cv2.imread(os.path.join(craft_res_dir, fname)))
 		tmp_txt = open(os.path.join(craft_res_dir, fname[:len(fname)-3]+"txt"),"r").read().split("\n")[:-1]
 		tmp_txt = [line.split(",") for line in tmp_txt]
 		tmp_bxs = [[[int(line[i]),int(line[i+1])] for i,val in enumerate(line) if int(i)%2==0] for line in tmp_txt ]
-		boxes.append(tmp_bxs)
+		boxes[fname[4:len(fname)-4]] = tmp_bxs
 
 # get the original images to crop them
 # org_img_dir = "/Users/jasonli/Desktop/BU/Junior/Spring2021/CS791/sandbox/test_models/CRAFT-pytorch-master/in_data"
-org_img_dir = "/projectnb/sparkgrp/ml-herbarium-grp/ml-herbarium-data/scraped-data/20220405-005447" # "/Users/jasonli/Desktop/BU/Junior/Spring2021/CS791/sandbox/herb_dat/imgs"
-imgs = []
-fnames = []
-for fname in sorted(os.listdir(org_img_dir)):
-	#new
+org_img_dir = "/projectnb/sparkgrp/ml-herbarium-grp/ml-herbarium-data/scraped-data/20220405-005447"# "/Users/jasonli/Desktop/BU/Junior/Spring2021/CS791/sandbox/herb_dat/imgs"
+imgs = {}
+for key in boxes:
 	if ".jpg" in fname:
-		imgs.append(cv2.imread(os.path.join(org_img_dir, fname),1))
-	#endnew
-		fnames.append(fname)
-n_imgs = len(imgs)
-print(n_imgs)
+		imgs[key]=cv2.imread(os.path.join(org_img_dir, key+".jpg"))
 # print(fnames)
 
 
@@ -61,13 +55,13 @@ def has_overlap(b1,b2):
 	return None
 
 # expands boxes according to input margins
-def expand_boxes(boxes, diff_axes=False, mx=20, my=40, m=4):
+def expand_boxes(bxs, diff_axes=False, mx=20, my=40, m=4):
 	if not diff_axes:
 		mx = m
 		my = m
 		
 	boxes_exp = []
-	for box in boxes:
+	for box in bxs:
 		tl, tr, br, bl = box
 		newtl = [tl[0]-mx, tl[1]-my]
 		newtr = [tr[0]+mx, tr[1]-my]
@@ -80,7 +74,6 @@ def expand_boxes(boxes, diff_axes=False, mx=20, my=40, m=4):
 		
 # combines the expanded boxes into a large box (presumably the label)
 def combine_boxes(boxes):
-	count = len(boxes)
 	newboxes = []
 
 	for box in boxes:
@@ -100,29 +93,15 @@ def combine_boxes(boxes):
 # sorts the boxes by area smallest to largest
 def sort_by_size(boxes):
 	areas = []
-	#new
-	if not boxes:
-		print("ha")
-		#return (sorted(areas)),leave=True
-	#endnew
 	for bx in boxes:
 		tl,tr,br,bl = bx
 		areas.append((br[0]-tl[0])*(br[1]-tl[1]))
 		
-	return [x for x in sorted(zip(areas,boxes))]
+	return [x for _,x in sorted(zip(areas,boxes))]
 
 # crops the labels from the images
 def crop_labels(img, box):
-	print("&&&&&&&&&")
-	print(box)
-	#tl, tr, br, bl = box
-	#new
-	tl = box[1][0]
-	tr = box[1][1]
-	br = box[1][2]
-	bl = box[1][3]
-	
-	#endnew
+	tl,tr,br,bl = box
 	return img[tl[1]:bl[1],tl[0]:tr[0]]
 
 # gets the lines of the image based on text boxes from craft
@@ -162,79 +141,43 @@ def get_lines(boxes, vert_m=12):
 # crops out images of the lines 
 def crop_lines(boxes, imgs):
 	line_crops = []
-	errors = 0
 	for i,bxs in enumerate(boxes):
-		try:
-			img_lines = []
-			for bx in bxs:
-				t1,t2,t3,t4 = bx
-				tmp_crop = imgs[i][t1[1]:t4[1],t1[0]:t2[0]]
-				if len(tmp_crop) > 0 and len(tmp_crop[0]) > 0:
-					img_lines.append(tmp_crop)
-			
-			line_crops.append(img_lines)
-		except:
-			errors += 1
-			print("error in crop_lines at index: ", i)
-			print(bxs)
-			print(i)
-			print(len(boxes))
-			print(len(imgs))
-			print(errors)
+		img_lines = []
+		for bx in bxs:
+			t1,t2,t3,t4 = bx
+			tmp_crop = imgs[i][t1[1]:t4[1],t1[0]:t2[0]]
+			if len(tmp_crop) > 0 and len(tmp_crop[0]) > 0:
+				img_lines.append(tmp_crop)
+		
+		line_crops.append(img_lines)
 	return line_crops
 
 
 # segment the labels
-boxes_exp = [expand_boxes(bxs, diff_axes=True) for bxs in boxes] # expand boxes
-#for bx in boxes_exp:
-#	print(bx)
-	
-boxes_comb = [combine_boxes(bxs) for bxs in boxes_exp] # combine the expanded boxes
-#print("#################")
-# new...
-boxes_comb_sorted = []
-for bxs in boxes_comb:
-	if not bxs:
-		continue
-	else:
-		x = list(reversed(sort_by_size(bxs)))[0]
-		boxes_comb_sorted.append(x)
+boxes_exp = {key: expand_boxes(bxs, diff_axes=True) for key, bxs in boxes.items()} # expand boxes
+boxes_comb = {key: combine_boxes(bxs) for key, bxs in boxes_exp.items()} # combine the expanded boxes
+boxes_comb_sorted = {key: list(reversed(sort_by_size(bxs)))[0] for key, bxs in boxes_comb.items()} # sort them and take the largest box
+labels = {} # segment label
 
-#endnew
-#boxes_comb_sorted = [list((sort_by_size(bxs)))[0] for bxs in boxes_comb] # sort them and take the largest box
-labels = []
-#new...
-for i in range(n_imgs):
-	errors = 0
+for key, image in imgs.items():
 	try:
-		if i >= (len(boxes_comb_sorted)):
-			break
-		else:
-			labels.append(crop_labels(imgs[i],boxes_comb_sorted[i]))
+		labels[key]=crop_labels(image, boxes_comb_sorted[key])
 	except:
-		errors += 1
-		print("error in loop at line 207 at index: ", i)
-		continue
-#endnew
+		labels[key]=None
+		print("Error cropping label for image: ", key+".jpg")
 
-# segment the lines of text (used to feed into models like mxnet)
-lines = [get_lines(bxs) for bxs in boxes]
-lines = [expand_boxes(bxs) for bxs in lines]
-#print(lines)
-lines = crop_lines(lines, imgs)
+# # segment the lines of text (used to feed into models like mxnet)
+# lines = [get_lines(bxs) for bxs in boxes]
+# lines = [expand_boxes(bxs) for bxs in lines]
+# lines = crop_lines(lines, imgs)
 
 # save cropped labels
-timestr = time.strftime("%Y%m%d-%H%M%S")
-save_dir = "/projectnb/sparkgrp/ml-herbarium-grp/ml-herbarium-data/seg-results" + "/" + timestr + "/"
+save_dir = "/projectnb/sparkgrp/ml-herbarium-grp/ml-herbarium-data/seg-results/"
 if not os.path.exists(save_dir):
 	os.makedirs(save_dir)
 
-for i,label in enumerate(labels):
-	try:
-		plt.imsave(os.path.join(save_dir, fnames[i]+"_label.jpg"), label)
-	except:
-		print("error in saving label at index: ", i)
-		continue
+for key, label in labels.items():
+	plt.imsave(os.path.join(save_dir, key+"_label.jpg"), label)
 
 # for i,j in enumerate(lines[0]):
 # 	cv2.imwrite(os.path.join(save_dir, "test"+str(i)+".jpg"), j)
