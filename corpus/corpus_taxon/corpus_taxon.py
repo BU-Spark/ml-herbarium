d2d673dc01e7218883e1779d1c5ca5eefc590fdb
Angeline Tseng <angelinecct3@gmail.com>
2022-04-19 20:33:37 -0400

    modify the code to take care of duplicates

--- corpus/corpus_taxon/corpus_taxon.py
+++ corpus/corpus_taxon/corpus_taxon.py
@@ -4,6 +4,7 @@ import zipfile
 import os
 import pandas as pd
 import shutil
+import collections
 
 
 ### globals
@@ -13,6 +14,7 @@ csv_dir = 'ipni2/ipni.csv'
 csv = 'ipni.csv'
 zip_file = 'ipni.zip'
 corpus_taxon = 'corpus_taxon.txt'
+duplicates = 'duplicates_taxon.txt'
 
 
 ### Download Dataset
@@ -26,16 +28,27 @@ if os.path.exists(output_dir + 'ipni2'):
     shutil.rmtree(output_dir + 'ipni2')
 with zipfile.ZipFile(output_dir + zip_file,'r') as zip_ref:
     zip_ref.extract(member=csv_dir, path=output_dir)
-#zf = zipfile.ZipFile(output_dir + zip_file)
+# zf = zipfile.ZipFile(output_dir + zip_file)
 df = pd.read_csv(csv_dir, header=None)
 
 
 ### Export Taxon Name to corpus-taxon.txt
-with open(output_dir + corpus_taxon, "w") as output_file:
-    [output_file.write(row + '\n') for row in df[1]]
-    output_file.close()
+# we need to take care of the duplicates in the dataset 
+list_taxon = df[1].to_list()
+list_taxon_no_dup = list(set(list_taxon))
 
-with open(output_dir + corpus_taxon, 'r') as fp:
-    x = len(fp.readlines())
-    print('\n' + 'Total lines:', x) 
+# print out the duplicates on duplicates_taxon.txt
+dup = [item for item, count in collections.Counter(list_taxon).items() if count > 1]
+with open(output_dir + duplicates, "w") as output_file:
+    for i in range(len(dup)):
+        output_file.write(dup[i] + '\n')
+    output_file.close()
+print('\n' + 'Finish printing duplicates.')
+
+# print out the taxon names without duplicates 
+with open(output_dir + corpus_taxon, "w") as output_file:
+    for i in range(len(list_taxon_no_dup)):
+        output_file.write(list_taxon_no_dup[i] + '\n')
+    output_file.close()
+print('Finish printing taxon names.')
 
