8520161dcadf53997f5b7dad4cf3db6d66816e1b
Eamon Niknafs <en@bu.edu>
2022-04-27 17:27:37 -0400

    updated training images

--- transcription/handwriting_tesseract_training/convert_imgs_to_tif.py
+++ transcription/handwriting_tesseract_training/convert_imgs_to_tif.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import os
 
-images = os.listdir("/usr4/ugrad/en/ml-herbarium/transcription/handwriting_tesseract_training/images")
+images = os.listdir("images")
 for image_path in images:
-    img = Image.open("/usr4/ugrad/en/ml-herbarium/transcription/handwriting_tesseract_training/images/" + image_path)
-    img.save("/usr4/ugrad/en/ml-herbarium/transcription/handwriting_tesseract_training/images/" + image_path.split(".")[0] + ".tif", 'TIFF')
-    os.remove("/usr4/ugrad/en/ml-herbarium/transcription/handwriting_tesseract_training/images/" + image_path)
+    img = Image.open("images/" + image_path)
+    img.save("images/" + image_path.split(".")[0] + ".tif", 'TIFF')
+    os.remove("images/" + image_path)
