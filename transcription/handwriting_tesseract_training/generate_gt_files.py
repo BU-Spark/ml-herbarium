b7e9da1b2d2e3c38083790697dc0d788cefe3159
Eamon Niknafs <en@bu.edu>
2022-04-27 17:24:25 -0400

    fixed gt generator

--- transcription/handwriting_tesseract_training/generate_gt_files.py
+++ transcription/handwriting_tesseract_training/generate_gt_files.py
@@ -4,12 +4,13 @@ import shutil
 # Read ground_truth.txt
 with open("ground_truth.txt", "r") as f:
     lines = f.readlines()
+    # Delete and recreate existing gt directory
+    if os.path.exists("gt"):
+        shutil.rmtree("gt")
+    os.mkdir("gt")
     for line in lines:
-        # Delete and recreate existing gt directory
-        if os.path.exists("gt"):
-            shutil.rmtree("gt")
-        os.mkdir("gt")
         # Save each line to a new file
         split = line.split(" ")
         with open("gt/" + split[0] + ".gt.txt", "w") as f2:
-            f2.write(line[8:])
+            out = " ".join(split[8:])
+            f2.write(out)
