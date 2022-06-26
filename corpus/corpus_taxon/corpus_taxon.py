dcc3041113bbf3f4fd681d418b81f582d8d4080e
angietseng <angelinecct3@gmail.com>
2022-06-26 11:45:38 -0400

    generate libraries for possible genus&species

--- corpus/corpus_taxon/corpus_taxon.py
+++ corpus/corpus_taxon/corpus_taxon.py
@@ -5,10 +5,12 @@ import os
 import pandas as pd
 import shutil
 import collections
+import pickle 
 
 
 ### globals
 dataset_url = 'https://hosted-datasets.gbif.org/datasets/ipni.zip'
+dataset_dir = '/projectnb/sparkgrp/ml-herbarium-grp/ml-herbarium-data/corpus_taxon/'
 output_dir = '/projectnb/sparkgrp/ml-herbarium-grp/ml-herbarium-angeline1/ml-herbarium/corpus/corpus_taxon/'
 csv_dir = 'ipni2/ipni.csv'
 csv = 'ipni.csv'
@@ -18,17 +20,17 @@ duplicates = 'duplicates_taxon.txt'
 
 
 ### Download Dataset
-if os.path.exists(output_dir + zip_file):
-    os.remove(output_dir + zip_file)
-wget.download(dataset_url, out=output_dir + zip_file)
+# if os.path.exists(dataset_dir + zip_file):
+#     os.remove(dataset_dir + zip_file)
+# wget.download(dataset_url, out=dataset_dir + zip_file)
 
 
 ### Extract .csv from .zip
-if os.path.exists(output_dir + 'ipni2'):
-    shutil.rmtree(output_dir + 'ipni2')
-with zipfile.ZipFile(output_dir + zip_file,'r') as zip_ref:
+if os.path.exists(dataset_dir + 'ipni2'):
+    shutil.rmtree(dataset_dir + 'ipni2')
+with zipfile.ZipFile(dataset_dir + zip_file,'r') as zip_ref:
     zip_ref.extract(member=csv_dir, path=output_dir)
-# zf = zipfile.ZipFile(output_dir + zip_file)
+# zf = zipfile.ZipFile(dataset_dir + zip_file)
 df = pd.read_csv(csv_dir, header=None)
 
 
@@ -47,8 +49,32 @@ print('\n' + 'Finish printing duplicates.')
 
 # print out the taxon names without duplicates 
 with open(output_dir + corpus_taxon, "w") as output_file:
-    for i in range(len(list_taxon_no_dup)):
-        output_file.write(list_taxon_no_dup[i] + '\n')
-    output_file.close()
+    with open(output_dir + "single.txt", "w") as file:
+        dict_possible_species = {}
+        dict_possible_genus = {}
+        for i in range(len(list_taxon_no_dup)):
+            # get rid of single-word taxons
+            if ' ' in list_taxon_no_dup[i]:
+                output_file.write(list_taxon_no_dup[i] + '\n')
+                genus = list_taxon_no_dup[i].split()[0]
+                species = "".join(list_taxon_no_dup[i].split()[1:])
+            
+                if genus not in dict_possible_species:
+                    dict_possible_species[genus] = [species]
+                else:
+                    dict_possible_species[genus] += [species]
+
+                if species not in dict_possible_genus:
+                    dict_possible_genus[species] = [genus]
+                else:
+                    dict_possible_genus[species] += [genus]
+            else:
+                file.write(list_taxon_no_dup[i] + '\n')
+        with open(dataset_dir + 'output/possible_species.pkl', 'wb') as f:
+            pickle.dump(dict_possible_species, f) 
+        with open(dataset_dir + 'output/possible_genus.pkl', 'wb') as filee:
+            pickle.dump(dict_possible_genus, filee)     
+file.close()
+output_file.close()
 print('Finish printing taxon names.')
 
