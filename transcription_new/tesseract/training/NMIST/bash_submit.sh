db0ae91854b5df149cd633facb78372b8f72a86f
Angeline Tseng <angelinecct3@gmail.com>
2022-08-07 00:02:14 +0800

    Revert "ML-Herbarium: Feature - multithread transcribe labels from main"

--- transcription_new/tesseract/training/NMIST/bash_submit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-#$ -l gpus=1
-#$ -j y
-#$ -m ea
-#$ -l gpu_c=6.0
-
-module load python3/3.8.10
-python3 /projectnb/sparkgrp/ml-herbarium-grp/ml-herbarium-data/tesseract-training/MNIST/pre-processing.py
