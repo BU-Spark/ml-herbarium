f586f850db87350b325135a1d480846445c5e4a6
Eamon Niknafs <en@bu.edu>
2022-08-09 21:22:30 -0400

    Fixed issues from merge

--- transcription_new/tesseract/training/CVIT/unzip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-
-#$ -N "Unzip CVIT Dataset"       # Give job a name
-#$ -j y               # Merge the error and output streams into a single file
-#$ -m e               # Send email at end of job
-#$ -m a               # Send email at abort of job
-# -M "your@email.here" #Be sure to add a money sign after the hashtag
-
-tar -xf /projectnb/sparkgrp/ml-herbarium-grp/ml-herbarium-data/tesseract-training/training/CVIT/iiit-hws.tar.gz
