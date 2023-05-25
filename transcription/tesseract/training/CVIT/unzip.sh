c167fc4b2194c293a7f3372db266ab36214612ff
eamonniknafs <en@bu.edu>
2023-05-25 15:29:01 -0400

    [Chore] cleaned up folder structure and docs

--- transcription/tesseract/training/CVIT/unzip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-
-#$ -N "Unzip CVIT Dataset"       # Give job a name
-#$ -j y               # Merge the error and output streams into a single file
-#$ -m e               # Send email at end of job
-#$ -m a               # Send email at abort of job
-#$ -M "en@bu.edu"
-
-tar -xf /projectnb/sparkgrp/ml-herbarium-grp/ml-herbarium-data/tesseract-training/training/CVIT/iiit-hws.tar.gz
